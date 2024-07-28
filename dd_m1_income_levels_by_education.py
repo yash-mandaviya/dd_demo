3bc4a510d8519cdb5b4c5d407f688b45d01b8d1e
yash-mandaviya <ymmandaviya1234@gmail.com>
2024-07-28 18:37:16 -0400

    update m1

--- dd_m1_income_levels_by_education.py
+++ dd_m1_income_levels_by_education.py
@@ -56,8 +56,9 @@ sns.barplot(x='Both Sexes', y='Education level', color='blue', data=data, ax=ax)
 st.pyplot(fig)
 
 st.subheader('Density plot of wages by type of work')
+fig, ax = plt.subplots()    
 sns.displot(data=data, x='Both Sexes', hue='Type of work', kind='kde')
-st.pyplot()
+st.pyplot(fig)
 
 st.subheader('Density plot of wages by type of work for Male')
 sns.displot(data=data, x='  Male', hue='Type of work', kind='kde')
